434a883a0cc0ec1e4eb6d2f59799f4b331cd1cc5
Max B <mcbernd@outlook.com>
2025-05-21 16:06:49 +0200

    client und optionen aus helper zurückgeben (unfertig)

---
+++ MQTT_WinForms/MQTT/MqttClientHelper.cs
@@ -0,0 +1,53 @@
+﻿using MQTT_WinForms.BASE;
+using MQTTnet;
+
+namespace MQTT_WinForms.MQTT
+{
+    public static class MqttClientHelper
+    {
+        public class MQTTWrapper
+        {
+            public IMqttClient? Client { get; set; }
+
+            public MqttClientOptions? Options { get; set; }
+        }
+
+        public static MQTTWrapper ConnectToMqttServer(ConnectionData connectionData)
+        {
+            #region Verbindungsinformationen übernehmen
+            var options = new MqttClientOptionsBuilder();
+            if (connectionData.Username != null || connectionData.Username != string.Empty)
+            {
+                options
+                .WithClientId(connectionData.ClientID)
+                .WithTcpServer(connectionData.Address, connectionData.Port)
+                .WithCredentials(connectionData.Username, connectionData.Password)
+                .WithCleanSession()
+                .Build();
+            }
+            else
+            {
+
+                options
+                .WithClientId(connectionData.ClientID)
+                .WithTcpServer(connectionData.Address, connectionData.Port)
+                .WithCleanSession()
+                .Build();
+            }
+            #endregion
+
+            MqttClientFactory mqttFactory = new MqttClientFactory();
+            IMqttClient mqttClient = mqttFactory.CreateMqttClient();
+
+            return new MQTTWrapper
+            {
+                Client = mqttClient,
+                Options = options.Build()
+            };
+            
+
+
+        }
+
+    }
+}
